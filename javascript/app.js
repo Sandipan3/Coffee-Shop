8c68d539316e4b532b9c3ba8e434a126bdf53171
Sandipan Jha <80733598+Sandipan3@users.noreply.github.com>
2024-08-22 21:42:41 +0530

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,48 @@
+/* navbar */
+console.log("LINK SUCCESSFUL");
+
+const menu = document.getElementById("menu-list");
+const navbar = document.getElementById("navbar");
+menu.style.maxHeight = "0rem";
+navbar.style.height = "auto";
+
+
+const toggleMenu = () => {
+    if (menu.style.maxHeight === "0rem") {
+        menu.style.maxHeight = "35rem"
+        navbar.style.height = "12rem";
+    }
+    else {
+        menu.style.maxHeight = "0rem"
+        navbar.style.height = "auto";
+    }
+};
+
+document.querySelector(".menu-icon").addEventListener("click", toggleMenu);
+
+/* different menu list buttons and site navigation */
+
+
+const exploreBtn = document.querySelector(".header__button");
+const learnMoreBtn = document.querySelector(".about__button");
+const inventoryBtn = document.querySelector(".products__description_button");
+const readMoreBtn = document.querySelectorAll(".services-card__button");
+
+const gotoPage = (url) => {
+    window.location.href = url;
+}
+
+
+exploreBtn.addEventListener("click", () => gotoPage("services.html"));
+learnMoreBtn.addEventListener("click", () => gotoPage("about.html"));
+inventoryBtn.addEventListener("click", () => gotoPage("products.html"));
+readMoreBtn.addEventListener("click", () => gotoPage("products.html"));
+
+
+
+
+
+
+
+
+

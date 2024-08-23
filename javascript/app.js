db69f8016547a3f572747ca09d6ff386c91e27cf
Sandipan Jha <80733598+Sandipan3@users.noreply.github.com>
2024-08-23 13:29:03 +0530

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -39,7 +39,115 @@ inventoryBtn.addEventListener("click", () => gotoPage("products.html"));
 
 readMoreBtn.forEach((btn) => {
     btn.addEventListener("click", () => gotoPage("products.html"))
-})
+});
+
+/* Scroll Reveal */
+const skillsItems = document.querySelectorAll('.skills__item');
+
+ScrollReveal().reveal(skillsItems, {
+    distance: '3rem',
+    origin: 'bottom',
+    duration: 1000,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+const aboutImgContainer = document.querySelector(".about__img-container");
+
+ScrollReveal().reveal(aboutImgContainer, {
+    distance: '5rem',
+    origin: 'left',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+const aboutCard = document.querySelector(".about__card");
+
+ScrollReveal().reveal(aboutCard, {
+    distance: '5rem',
+    origin: 'right',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+const product = document.querySelector(".products");
+
+ScrollReveal().reveal(product.children, {
+    scale: 0.5,
+    duration: 1000,
+    easing: 'ease-in-out',
+    interval: 200,
+    opacity: 0,
+    reset: true
+});
+
+const servicesHeading = document.querySelector(".services-heading");
+
+ScrollReveal().reveal(servicesHeading.firstElementChild, {
+    distance: '5rem',
+    origin: 'right',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+ScrollReveal().reveal(servicesHeading.lastElementChild, {
+    distance: '5rem',
+    origin: 'left',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+
+const servicesCard = document.querySelectorAll(".services-card");
+
+ScrollReveal().reveal(servicesCard, {
+    rotate: { x: 0, y: 100, z: 0 },
+    duration: 1000,
+    easing: 'ease-in-out',
+    interval: 200,
+    reset: true
+});
+/* services card hover scale up gets disabled so I fixed that is js */
+servicesCard.forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = 'scale(1.05)';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'scale(1)';
+    });
+});
+
+
+const contactUsText = document.querySelector(".contact-us__details");
+
+ScrollReveal().reveal(contactUsText, {
+    distance: '7rem',
+    origin: 'right',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+const contactUsForm = document.querySelector(".contact-us__form");
+
+ScrollReveal().reveal(contactUsForm, {
+    distance: '7rem',
+    origin: 'left',
+    duration: 1500,
+    interval: 100,
+    easing: 'ease-in-out',
+    reset: true
+});
 
 
 
